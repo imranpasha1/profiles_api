b45995f705aa4ae8605e2ac73c8b9a1ed3c9ddb0
imran.pasha1 <imran.pasha1@nutanix.com>
2023-06-30 19:41:25 +0530

    added 2 api GET endpoints

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+import uuid
+
+class HelloApiView(APIView):
+    """Test API View"""
+    
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'User HTTP methods get',
+            'User HTTP methods post',
+            'User HTTP methods patch',
+            'User HTTP methods delete'
+        ]
+        
+        return Response({'message': "Hello World", "an_apiview": an_apiview})
+
+
+class Runbooks(APIView):
+    """Test Runbooks API"""
+    
+    def get(self, request, foramt=None):
+        """Returns runbook api response"""
+        
+        return Response({"message": "server response", "entity": uuid.uuid4()})
